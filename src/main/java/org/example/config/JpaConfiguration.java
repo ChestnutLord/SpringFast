cb7761530fa80972f87e03b00fad23a2ea778b6d
Dmitriy Gvozdenko <textdmitriy@gmail.com>
2025-05-17 09:42:04 +0300

    Commit

--- src/main/java/org/example/config/JpaConfiguration.java
+++ src/main/java/org/example/config/JpaConfiguration.java
@@ -2,6 +2,8 @@ package org.example.config;
 
 import jakarta.annotation.PostConstruct;
 import org.example.config.condition.JpaCondition;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.database.entity.Company;
 import org.example.database.repository.CrudRepository;
 import org.example.database.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +12,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    @Qualifier("CR_From_class")
     private final CrudRepository<Integer, Company> crudRepository;
 }
