000d65ae85856128230745909da512a9831f4a6a
Dmitriy Gvozdenko <textdmitriy@gmail.com>
2025-05-19 16:07:37 +0300

    Конец SpringBoot. @ConfigurationProperties и @ConfigurationPropertiesScan

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import org.example.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
 
         System.out.println("BeanDefinitionCount "+context.getBeanDefinitionCount());
+        System.out.println(context.getBean(DatabaseProperties.class));
     }
 }
--- src/main/java/org/example/config/JpaConfiguration.java
+++ src/main/java/org/example/config/JpaConfiguration.java
@@ -4,12 +4,21 @@ import jakarta.annotation.PostConstruct;
 import org.example.config.condition.JpaCondition;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Conditional(JpaCondition.class)
 @Configuration
 public class JpaConfiguration {
+
+//    @Bean
+//    // название метода будет id бина
+//    @ConfigurationProperties(prefix = "db")
+//    public DatabaseProperties databaseProperties() {
+//        return new DatabaseProperties();
+//    }
 
     @PostConstruct
     public void init() {
